b2d7e3330b9f9b946b16d3d68bb7a8544268820c
Julio Lima <julio.lima@lighthouseit.com.br>
2025-09-20 18:39:40 -0300

    commit

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\OFFICE TOTAL\\Desktop\\ProjetosFaculdade\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\OFFICE TOTAL\\Desktop\\ProjetosFaculdade\\nutricao\\app_nutricao" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\OFFICE TOTAL\\Desktop\\ProjetosFaculdade\\flutter"
+  "PROJECT_DIR=C:\\Users\\OFFICE TOTAL\\Desktop\\ProjetosFaculdade\\nutricao\\app_nutricao"
+  "FLUTTER_ROOT=C:\\Users\\OFFICE TOTAL\\Desktop\\ProjetosFaculdade\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\OFFICE TOTAL\\Desktop\\ProjetosFaculdade\\nutricao\\app_nutricao\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\OFFICE TOTAL\\Desktop\\ProjetosFaculdade\\nutricao\\app_nutricao"
+  "FLUTTER_TARGET=C:\\Users\\OFFICE TOTAL\\Desktop\\ProjetosFaculdade\\nutricao\\app_nutricao\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzUuNA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZDY5M2I0YjlkYg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049YzI5ODA5MTM1MQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjI="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\OFFICE TOTAL\\Desktop\\ProjetosFaculdade\\nutricao\\app_nutricao\\.dart_tool\\package_config.json"
+)
